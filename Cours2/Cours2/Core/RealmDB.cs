c532181a571959a1d379534b6c002614d5cf7fa5
Antoine Poussard <antoinepoussard@MacBook-Pro-de-Antoine.local>
2018-01-22 12:49:29 +0100

    Realm DB added + Task declaration

---
+++ Cours2/Cours2/Core/RealmDB.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Realms;
+
+namespace Cours2.Core
+{
+    public enum PrioriteEnum { Bas, Normal, Haut }
+    public class RealmDB : RealmObject
+    {
+        public RealmDB()
+        {
+        }
+        public string Nom { get; set; }
+        public string Descritption { get; set; }
+        public string enumDescription { get; set; }
+
+        public void saveEnum(PrioriteEnum val)
+        {
+            this.enumDescription = val.ToString();
+        }
+
+        public PrioriteEnum getEnum()
+        {
+            PrioriteEnum tmpEnum = (PrioriteEnum)Enum.Parse(typeof(PrioriteEnum), enumDescription);
+            return tmpEnum;
+        }
+    }
+}
